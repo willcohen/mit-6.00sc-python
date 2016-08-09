e6943dcd433df8b254559029c69f305396812ad8
Will Cohen <willcohen@users.noreply.github.com>
2016-08-09 13:48:55 -0400

    Raw input for problem set 0

--- ps0.py
+++ ps0.py
@@ -1,5 +1,5 @@
 # Problem Set 0
 
-dob = input('Enter your date of birth: ')
-lastname = input('Enter your last name: ')
-print(lastname, ' ', dob)
+dob = raw_input('Enter your date of birth: ')
+lastname = raw_input('Enter your last name: ')
+print lastname, ' ', dob
